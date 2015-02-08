42f98b625d1ba6828b92d1de0ffedbeb77f9c752
dincho <dincho@users.noreply.github.com>
2015-02-08 12:56:57 +0200

    install ansible roles with generic provisioner script too

--- provisioning/generic-provision.sh
+++ provisioning/generic-provision.sh
@@ -3,5 +3,6 @@ sudo apt-get -y install software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install ansible
+sudo ansible-galaxy install nbz4live.php-fpm jdauphant.nginx f500.project_deploy
 cd /home/vagrant/$1/provisioning
 ansible-playbook -i inventary/dev --connection=local devservers.yml
